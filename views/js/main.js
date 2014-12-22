f3f334679cec2c48251c6a1dbbbf79c115bde08d
Jay W Johnson <jwj@boun.cr>
2014-12-22 17:41:30 +0800

    Pulled a repeated calculation out of for loop.

--- views/js/main.js
+++ views/js/main.js
@@ -504,8 +504,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var offset = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(offset + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
