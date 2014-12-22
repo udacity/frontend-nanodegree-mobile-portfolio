36b5aa3f13ebc4a599014324f113e39981b48462
Jay W Johnson <jwj@boun.cr>
2014-12-22 17:01:14 +0800

    testing timing of png

--- views/js/main.js
+++ views/js/main.js
@@ -529,7 +529,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.webp";
+    elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
