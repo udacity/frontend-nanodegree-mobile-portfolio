9efa023dd254c1d22bdae8de41bb2d79200c2dc3
tarheelsrule44 <tyler.woods44@gmail.com>
2015-02-17 20:15:23 -0500

    edit for loop 1

--- views/js/main.js
+++ views/js/main.js
@@ -450,9 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+  	var pizzaContainers = document.querySelectorAll(".randomPizzaContainer");
+  	var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {      
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
