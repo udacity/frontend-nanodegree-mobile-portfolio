72c4618e6f3336397ca69eda7c1445ecbf13a15a
allbad <neil_allgood@hotmail.com>
2015-02-23 12:43:08 +0000

    Optimised sliding pizza function

--- views/js/main.js
+++ views/js/main.js
@@ -509,8 +509,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var cachedScrollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((cachedScrollTop/1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
