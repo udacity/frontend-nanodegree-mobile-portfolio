4cd1bd41d860fe518a167b2f7bfa7f2d21d2c0e2
allbad <neil_allgood@hotmail.com>
2015-02-23 21:24:10 +0000

    Optimise JS in views using grunt-uglify

---
+++ js/perfmatters.min.js
@@ -0,0 +1,1 @@
+function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading,c=a.domComplete-a.domLoading,d=document.getElementById("crp-stats");d.textContent="DCL: "+b+"ms, onload: "+c+"ms"}window.addEventListener("load",function(){logCRP()});
