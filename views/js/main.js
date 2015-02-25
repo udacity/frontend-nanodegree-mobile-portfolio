52b4fa417de95c2253e182acd81dd68f1485a111
allbad <neil_allgood@hotmail.com>
2015-02-25 16:57:53 +0000

    Standardise pizza sizes

--- views/js/main.js
+++ views/js/main.js
@@ -378,7 +378,7 @@ var pizzaElementGenerator = function(i) {
   pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
   pizzaImageContainer.classList.add("col-md-6");
 
-  pizzaImage.src = "images/pizza_sml.png";
+  pizzaImage.src = "images/pizza.png";
   pizzaImage.classList.add("img-responsive");
   pizzaImageContainer.appendChild(pizzaImage);
   pizzaContainer.appendChild(pizzaImageContainer);
@@ -542,7 +542,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 32; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza_sml.png";
+    elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73px";
     elem.basicLeft = (i % cols) * s;
