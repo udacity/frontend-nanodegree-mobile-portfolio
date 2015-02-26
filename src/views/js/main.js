19b0ff11d6871762534e45e1f9d95ff9e28ceee0
allbad <neil_allgood@hotmail.com>
2015-02-26 14:47:54 +0000

    Process with grunt and tidy up

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -476,7 +476,7 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
+for (var i = 2; i < 81; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
