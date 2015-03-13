bbb7348e8c8d7565adcb38e3fe7b2574daf862a6
Renee Cooper <reneemillercooper@gmail.com>
2015-03-13 13:22:45 -0700

    Optimize Part 1 of P4

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -12,3 +12,31 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+
+window.addEventListener("load", function (w, g) {
+
+    w['GoogleAnalyticsObject'] = g;
+    w[g] = w[g] || function () { (w[g].q = w[g].q || []).push(arguments); }; w[g].l = 1 * new Date();
+
+
+    // TODO: replace with your Google Analytics profile ID.
+    g('create', 'UA-XXXX-Y');
+    g('send', 'pageview');
+});  (window, 'ga');
+
+WebFontConfig = {
+    google: { families: ['Open+Sans:400'] }
+};
+
+window.addEventListener("load", function () { delayLoadWebFont(); });
+
+var delayLoadWebFont = function () {
+    var wf = document.createElement('script');
+    wf.src = ('https:' === document.location.protocol ? 'https' : 'http') +
+              '://ajax.googleapis.com/ajax/libs/webfont/1.5.6/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+};
+
