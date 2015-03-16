04f808003d3c1b012167ae52346aafa81e93a84b
Stefan Worzalla <stewo1337@gmail.com>
2015-03-16 21:04:41 +0100

    typo

--- views/js/main.js
+++ views/js/main.js
@@ -516,7 +516,7 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
     //var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     //console.log("item: "+i);
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + 100 * phase[i] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
