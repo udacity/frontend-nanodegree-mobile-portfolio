850f15e32779504f0fa286f1a8015c3f3778e3f3
dingGator <kim@chucrew.net>
2015-06-24 19:55:47 -0400

    moving pizza disappear so back out change
    moving pizza disappear so back out changes

--- views/js/main.js
+++ views/js/main.js
@@ -533,7 +533,8 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    document.querySelector("#movingPizzas1").appendChild(elem);
+
   }
-  document.querySelector("#movingPizzas1").appendChild(elem);
   updatePositions();
 });
