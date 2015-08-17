0021c8888cd3ba974cf3876e08e77182f3b19c70
MICHAEL HAZANI <123mickey@gmail.com>
2015-08-17 10:51:04 -0700

    add media query to print stylesheet move all CSS to inline remove unnecessary call to google fonts, move to inline minify HTML

---
+++ js/min/perfmatters-min.js
@@ -0,0 +1,1 @@
+function logCRP(){var o=window.performance.timing,n=o.domContentLoadedEventStart-o.domLoading,t=o.domComplete-o.domLoading,d=document.getElementById("crp-stats");d.textContent="DCL: "+n+"ms, onload: "+t+"ms"}window.addEventListener("load",function(o){logCRP()});
