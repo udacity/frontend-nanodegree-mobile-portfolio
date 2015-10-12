a766867f00256f2e28c917e61cfda988924c3933
Dave Burdick <daveburdick@gmail.com>
2015-10-11 20:44:18 -0600

    Reduce number of pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -525,7 +525,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 20; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
