bf17b7c104b7e11b6915cc2069a5e031377a69b0
ozkarlav <ozkarlav@yahoo.com>
2016-03-11 18:00:43 -0500

    Fixed performance on website and compressed images for better rate on PageSpeed Insight

--- views/js/main.js
+++ views/js/main.js
@@ -491,6 +491,9 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  };
+
+  for (var i = 0; i < items.length; i++) {
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
