f912d21bfec1de917aa06844317f6e9e5f2b4e05
slkcoin <xcoin@me.com>
2016-04-17 06:28:46 -0700

    Major gulp integration is finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
 var gulp = require('gulp'),
-    ngrok = require('ngrok'),
+    gls = require('gulp-live-server'),
     uglify = require('gulp-uglify'),
     csso = require('gulp-csso'),
     htmlmin = require('gulp-htmlmin')
     replace = require('gulp-html-replace')
     rename = require('gulp-rename');
 
-gulp.task('default', ['host','scripts', 'styles','replace', 'html']);
-
-gulp.task('host', function(){
-  ngrok.connect(function (err, url) {});
-});
+gulp.task('start', ['scripts', 'styles','html']);
 
 gulp.task('scripts', function(){
   gulp.src('src/js/*.js')
@@ -21,6 +17,15 @@ gulp.task('scripts', function(){
       path.extname = ".js"
     }))
     .pipe(gulp.dest('./dist'));
+
+    gulp.src('src/views/js/*.js')
+      .pipe(uglify())
+      .pipe(rename(function(path) {
+        path.dirname += "/views/js";
+        path.basename += ".min";
+        path.extname = ".js"
+      }))
+      .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('styles', function(){
@@ -29,23 +34,50 @@ gulp.task('styles', function(){
     .pipe(rename(function(path) {
       path.dirname += "/css";
       path.basename += ".min";
-      path.extname = ".css"
+      path.extname = ".css";
     }))
     .pipe(gulp.dest('./dist'));
+
+  gulp.src('src/views/css/*.css')
+  .pipe(csso())
+  .pipe(rename(function(path) {
+    path.dirname += "/views/css";
+    path.basename += ".min";
+    path.extname = ".css"
+  }))
+  .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('html', function(){
   gulp.src('src/*.html')
-    .pipe(htmlmin())
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('replace', function(){
-  gulp.src('src/index.html')
     .pipe(replace({
       'css': ['css/style.min.css', 'css/print.min.css'],
       'js': 'js/perfmatters.min.js'
     }))
-  .pipe(gulp.dest('./dist'));
+    .pipe(htmlmin())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist'));
+
+  gulp.src('src/views/*.html')
+    .pipe(replace({
+      'css': ['css/style.min.css', 'css/print.min.css'],
+      'js': 'js/perfmatters.min.js'
+    }))
+    .pipe(htmlmin())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./dist/views/'));
+});
+
+gulp.task('dev', function(){
+  var server = gls.static('src',8000)
+  server.start();
+
+  gulp.watch(['src/css/*.css', 'src/*.html'], function (file) {
+    server.notify.apply(server, [file]);
+  });
+});
+
+gulp.task('serve', function(){
+  var server = gls.static('dist',8000)
+  server.start();
 });
