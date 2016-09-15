40658edf5483f7195e7bdfcd84f86068b07552c3
joshpson <josh.j.pearson@gmail.com>
2016-09-15 19:40:25 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -182,7 +182,6 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
-  requestAnimationFrame(updatePositions);
   frame++;
   window.performance.mark("mark_start_frame");
 
@@ -203,7 +202,7 @@ function updatePositions() {
 }
 
 // runs updatePositions on scroll
-window.addEventListener('scroll', requestAnimationFrame(updatePositions));
+window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
