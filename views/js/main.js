5e5c8e0e5fb2edda6be928c47c78bcb458bd04cd
joshpson <josh.j.pearson@gmail.com>
2016-09-15 20:16:26 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -53,17 +53,14 @@ randomPie.onmessage = function(e) {
     pizzaDescriptionContainer = document.createElement("div");
 
     pizzaContainer.classList.add("randomPizzaContainer");
-    pizzaContainer.style.width = "33.33%";
-    pizzaContainer.style.height = "325px";
+    pizzaDescriptionContainer.classList.add("pizzaDescriptionContainer");
     pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
-    pizzaImageContainer.style.width="35%";
 
     pizzaImage.src = "images/pizza.png";
     pizzaImage.classList.add("img-responsive");
     pizzaImageContainer.appendChild(pizzaImage);
     pizzaContainer.appendChild(pizzaImageContainer);
 
-    pizzaDescriptionContainer.style.width="65%";
 
     pizzaName = document.createElement("h4");
     pizzaName.innerHTML = e.data[i];
