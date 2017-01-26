5d80d72563b81be735265898be01022d0107531f
fyrku <fyrku@mail.ru>
2017-01-26 15:50:56 +0300

    https://github.com/fyrku/optimize.git

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -2,9 +2,9 @@
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
 function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
+  var t = window.performance.timing;
+  var dcl = t.domContentLoadedEventStart - t.domLoading;
+  var complete = t.domComplete - t.domLoading;
   var stats = document.getElementById("crp-stats");
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
