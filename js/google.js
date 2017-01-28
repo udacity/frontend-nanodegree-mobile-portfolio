744cb42061e27002184faab7869d99cb1f2311e3
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2017-01-28 15:53:13 +0300

    all

---
+++ js/google.js
@@ -0,0 +1,6 @@
+(function(w,g){w['GoogleAnalyticsObject']=g;
+               w[g]=w[g]||function(){(w[g].q=w[g].q||[]).push(arguments)};w[g].l=1*new Date();})(window,'ga');
+
+// Optional TODO: replace with your Google Analytics profile ID.
+ga('create', 'UA-XXXX-Y');
+ga('send', 'pageview');
