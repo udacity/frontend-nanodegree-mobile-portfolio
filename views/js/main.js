867ea06d2848f4c15da18db80212ee5004532d5e
Kate Zasulevich <katejidkova@gmail.com>
2017-01-28 19:47:14 +0300

    no message

--- views/js/main.js
+++ views/js/main.js
@@ -527,7 +527,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizzasmall.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
