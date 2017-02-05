3d7bb3596e44e591485948b99514367ac672865b
Artem Borutenko <artem.borutenko@gmail.com>
2017-02-05 13:13:49 +0300

    third

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,2 +1,1 @@
-function logCRP() {var t = window.performance.timing,dcl = t.domContentLoadedEventStart - t.domLoading,complete = t.domComplete - t.domLoading;var stats document.getElementById("crp-stats");stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
-}window.addEventListener("load", function(event) {logCRP();});
+function logCRP(){var a=window.performance.timing,b=a.domContentLoadedEventStart-a.domLoading,c=a.domComplete-a.domLoading,d=document.getElementById("crp-stats");d.textContent="DCL: "+b+"ms, onload: "+c+"ms"}window.addEventListener("load",function(a){logCRP()});
