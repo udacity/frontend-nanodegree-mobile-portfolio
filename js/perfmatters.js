994e52a26e81b700f87438a2e88c6d52d91e19fc
anubhuti monga <anubhuti.monga@gmail.com>
2017-05-27 23:42:51 +0530

    Delete perfmatters.js

--- js/perfmatters.js
+++
@@ -1,1 +0,0 @@
-function logCRP(){var t=window.performance.timing,dcl=t.domContentLoadedEventStart-t.domLoading,complete=t.domComplete-t.domLoading;var stats=document.getElementById("crp-stats");stats.textContent='DCL: '+dcl+'ms, onload: '+complete+'ms';}window.addEventListener("load",function(event){logCRP();});
