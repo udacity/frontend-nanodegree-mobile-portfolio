7c9741d34a817f4857f4f3c9d4605fef03379aea
kwdevs <morrison.devs@gmail.com>
2017-08-07 19:25:41 -0500

    fixed links
    broken links restored.
    
    gulpfile now includes browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
 // This task will optimize images and place them in dist directory
 gulp.task('images', function(){
@@ -9,4 +10,12 @@ gulp.task('images', function(){
   gulp.src('./views/images/**/*.*')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/views/images'))
+});
+// This task will implement browser sync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
