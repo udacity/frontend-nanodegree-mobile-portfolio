7565c82f222334c69ec0abd141ee1cc1f6f4622c
David Harris <david@david.harris@udacity.com>
2017-09-28 08:36:55 -0700

    modify document.body.scrollTop to correct for breaking change in latest version of Chrome

--- views/js/main.js
+++ views/js/main.js
@@ -503,7 +503,9 @@ function updatePositions() {
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    // document.body.scrollTop is no longer supported in Chrome.
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
